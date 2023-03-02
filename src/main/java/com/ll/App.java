017df7921d1d12d655c912b6c1a50149de87bdd3
tiactive <kimjaban410@gmail.com>
2023-03-02 11:26:24 +0900

    2단계 구현완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -11,10 +11,15 @@ public class App {
         System.out.println("== 명언 앱 ==");
 
         while(true) {
-            System.out.println("명령) ");
+            System.out.print("명령) ");
             String command = sc.nextLine().trim();
             if (command.equals("종료")) {
                 break;
+            } else if (command.equals("등록")) {
+                System.out.print("명언: ");
+                String content = sc.nextLine().trim();
+                System.out.print("작가: ");
+                String authorName = sc.nextLine().trim();
             }
         }
 
