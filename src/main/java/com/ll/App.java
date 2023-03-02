5f7d6e39cbb19f3d3d99287621ddfb12e3ed9b4a
tiactive <kimjaban410@gmail.com>
2023-03-02 11:51:58 +0900

    5단계 구현2부 목록출력

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -31,7 +31,13 @@ public class App {
                 System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                 lastWiseSayingId = id;
             } else if (command.equals("목록")) {
-                System.out.printf("생성된 명언 수: %d \n", wiseSayings.size());
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-".repeat(30));
+
+                for(int i = wiseSayings.size() - 1; i >= 0; i--) {
+                    WiseSaying wiseSaying = wiseSayings.get(i);
+                    System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthorName(), wiseSaying.getContent());
+                }
             }
         }
 
