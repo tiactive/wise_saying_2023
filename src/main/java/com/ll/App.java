1493196bf1a43a37daca8d5a6bb161f406cbae33
tiactive <kimjaban410@gmail.com>
2023-03-02 11:43:29 +0900

    5단계 구현완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -9,7 +11,8 @@ public class App {
     }
     public void run() {
         System.out.println("== 명언 앱 ==");
-        long lastWiseSayingId = 1;
+        long lastWiseSayingId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
         while(true) {
             System.out.print("명령) ");
@@ -17,11 +20,18 @@ public class App {
             if (command.equals("종료")) {
                 break;
             } else if (command.equals("등록")) {
+                long id = lastWiseSayingId + 1;
                 System.out.print("명언: ");
                 String content = sc.nextLine().trim();
                 System.out.print("작가: ");
                 String authorName = sc.nextLine().trim();
-                System.out.printf("%d번 명언이 등록되었습니다.\n", lastWiseSayingId++);
+                WiseSaying wiseSaying = new WiseSaying(id, content, authorName);
+                wiseSayings.add(wiseSaying);
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                lastWiseSayingId = id;
+            } else if (command.equals("목록")) {
+                System.out.printf("생성된 명언 수: %d \n", wiseSayings.size());
             }
         }
 
