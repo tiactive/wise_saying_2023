946d06dd07d765047baa219086a77f833e3dc2f2
tiactive <kimjaban410@gmail.com>
2023-03-02 13:52:26 +0900

    명령어 파싱

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,9 +4,7 @@ import com.ll.system.controller.SystemController;
 import com.ll.wiseSaying.controller.WiseSayingController;
 import com.ll.wiseSaying.entity.WiseSaying;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
     public void run() {
@@ -27,6 +25,21 @@ public class App {
                 wiseSayingController.write();
             } else if (command.equals("목록")) {
                 wiseSayingController.list();
+            } else if (command.startsWith("삭제")) {
+                // 정리 시작
+                // 삭제?id=1&authorName="홍길동) 이런 형식
+                String[] commandBits = command.split("\\?", 2);
+                String actionCode = commandBits[0];
+                Map<String, String> params = new HashMap<>();
+                String[] paramBits = commandBits[1].split("&");
+                for(String paramStr:paramBits) {
+                    String[] paramStrBits = paramStr.split("=", 2);
+                    String key = paramStrBits[0];
+                    String value = paramStrBits[1];
+                    params.put(key,value);
+                }
+                // 정리 끝
+                wiseSayingController.remove();
             }
         }
     }
--- src/main/java/com/ll/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/wiseSaying/controller/WiseSayingController.java
@@ -40,4 +40,8 @@ public class WiseSayingController {
             System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthorName(), wiseSaying.getContent());
         }
     }
+
+    public void remove() {
+
+    }
 }
